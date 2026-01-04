ab97ad50107c3fc2b0a9c38c800ad614544b7e10
 Marco Figueroa <marcofigueroa23@icloud.com>
2026-01-04 03:31:27 -0300

    adding updating and gitignore

--- script.sh
+++ script.sh
@@ -1,5 +1,9 @@
 #!/bin/bash 
 
+# PAQUETES NECESARIOS
+# 1) js-beautify
+# 2) sponge
+
 #Colours
 greenColour="\e[0;32m\033[1m"
 endColour="\033[0m\e[0m"
@@ -13,16 +17,20 @@ grayColour="\e[0;37m\033[1m"
 # Funcion de salida Ctrl + C
 function ctrl_c () {
   echo -e "\n\n${redColour}[i] Saliendo...${endColour}\n"
-  exit 1
+  tput cnorm && exit 1
 }
 
 # Crtl + C 
 trap ctrl_c INT
 
+# Variables
+main_url="https://htbmachines.github.io/bundle.js"
+
 # Funcion para el panel de ayuda
 function helpPanel () {
   echo -e "\n${yellowColour}[+]${endColour} ${grayColour}Para el correcto uso del script puedes proporcinar estos parametros:${endColour}\n"
   echo -e "\t${blueColour}[i]${endColour} ${redColour}-m <nombre>\t${endColour} ${grayColour}Busca por nombre de maquinas en [https://htbmachines.github.io/]${endColour}"
+  echo -e "\t${blueColour}[i]${endColour} ${redColour}-u\t\t${endColour} ${grayColour}Actualiza archivos locales de busqueda desde [https://htbmachines.github.io/]${endColour}"
   echo -e "\t${blueColour}[i]${endColour} ${redColour}-h\t\t${endColour} ${grayColour}Mostrar panel de ayuda${endColour}\n"
 }
 
@@ -32,13 +40,44 @@ function searchMachine () {
   echo -e "\n${greenColour}[+]${endColour} ${grayColour}Buscando maquina:${endColour} ${yellowColour}$machine${endColour}\n"
 }
 
+function updateFiles () {
+  tput civis
+  echo -e "\n${greenColour}[+]${endColour} ${grayColour}Actualizando base de archivos...${endColour}\n"
+  if [ ! -f bundle.js ]; then
+    echo -e "${redColour}[i]${endColour} ${yellowColour}El archivo bundle.js no existe${endColour}"
+    echo -e "${greenColour}[i]${endColour} ${grayColour}Comenzando descarga...${endColour}\n"
+    curl -s -X GET $main_url > bundle.js
+    js-beautify bundle.js |sponge bundle.js
+    echo -e "${greenColour}[i]${endColour} ${greenColour}Descarga completada${endColour}\n"
+  else
+    echo -e "${redColour}[i]${endColour} ${yellowColour}El archivo bundle.js existe${endColour}" 
+    echo -e "${greenColour}[i]${endColour} ${grayColour}Comparando cambios...${endColour}"
+    curl -s -X GET $main_url > bundle.tmp.js
+    js-beautify bundle.tmp.js |sponge bundle.tmp.js
+    echo -e "${greenColour}[i]${endColour} ${grayColour}Comparando Hash MD5...${endColour}\n"
+    md5TempHash=$(md5sum bundle.tmp.js |awk '{print $1}')
+    md5LocalHash=$(md5sum bundle.js |awk '{print $1}')
+    if [ "$md5LocalHash" = "$md5TempHash" ]; then
+      echo -e "${greenColour}[i]${endColour} ${greenColour}No hay actualizaciones${endColour}\n"
+      rm ./bundle.tmp.js
+    else
+      echo -e "${greenColour}[i]${endColour} ${grayColour}Hay actualizaciones pendientes${endColour}"
+      echo -e "${greenColour}[i]${endColour} ${grayColour}Actualizando archivo...${endColour}\n"
+      rm ./bundle.js && mv ./bundle.tmp.js ./bundle.js
+      echo -e "${greenColour}[i]${endColour} ${greenColour}Actualizacion completada${endColour}\n"
+    fi
+  fi
+  tput cnorm
+}
+
 # Indicador de parametros
 declare -i parameter_counter=0
 
 # Asignacion de parametros
-while getopts "m:h" arg; do
+while getopts "m:uh" arg; do
   case $arg in
     m) machineName=$OPTARG; let parameter_counter+=1;;
+    u) let parameter_counter+=2;;
     h) ;;
   esac
 done
@@ -46,6 +85,8 @@ done
 # Condicional para los parametros
 if [ $parameter_counter -eq 1 ]; then
   searchMachine $machineName
+elif [ $parameter_counter -eq 2 ]; then
+  updateFiles 
 else
   helpPanel
 fi
